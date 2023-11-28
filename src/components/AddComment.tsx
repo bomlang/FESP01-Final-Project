aaf9d3095e6217332c4dd5952f521e005bee5143
matt <wootae.jung@beautyselection.co.kr>
2023-11-28 15:49:13 +0900

    rebase 충돌 해결

--- src/components/AddComment.tsx
+++ src/components/AddComment.tsx
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import Button from "./Button"
-import { enterComment, filterComment, readComment } from "@api/commentApi"
-import { AddCommentProps } from "interface"
-import React, { useEffect, useRef, useState } from "react"
-
-function AddComment({ videoId, setCommentData }: AddCommentProps) {
-  const [isFocus, setIsFocus] = useState(false)
-  const [text, setText] = useState<string>("")
-  const textareaRef = useRef<HTMLTextAreaElement>(null)
-  const [isSubmit, setIsSubmit] = useState(false)
-
-  const handleInputFocus = () => {
-    setIsFocus(true)
-  }
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setText(e.target.value)
-=======
 import React, { useState } from "react"
 import Button from "./Button"
 
@@ -30,51 +11,10 @@ function AddComment() {
 
   const handleInputChange = (e: React.ChangeEvent<HTMLDivElement>) => {
     setText(e.target.innerText)
->>>>>>> 06e00f2 (v0.0.0 리뷰드려요. review라는 키워드를 검색하시면 찾을 수 있으실거에요.)
   }
 
   const handleCancel = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
-<<<<<<< HEAD
-    setIsFocus(false)
-    setText("")
-  }
-
-  const handleCommentSubmit = async () => {
-    await enterComment(text, videoId)
-    setIsSubmit((prevState) => !prevState)
-    setText("")
-  }
-
-  useEffect(() => {
-    const promiseData = filterComment(videoId)
-    promiseData
-      .then((comments) => {
-        setCommentData(comments || [])
-      })
-      .catch((error) => {
-        console.error("에러 발생: ", error)
-      })
-  }, [isSubmit])
-
-  const handleKeyDown = (e: React.KeyboardEvent) => {
-    if (e.key === "Enter") {
-      if (text === "") {
-        return
-      } else {
-        handleCommentSubmit()
-      }
-    }
-  }
-
-  // useEffect를 사용하여 텍스트가 변경될 때마다 높이를 조절합니다.
-  useEffect(() => {
-    if (textareaRef.current) {
-      textareaRef.current.style.height = "30px"
-      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`
-    }
-  }, [text])
-=======
     setState(false)
     setText("")
   }
@@ -84,7 +24,6 @@ function AddComment() {
     console.log("댓글 등록:", text)
     setText("")
   }
->>>>>>> 06e00f2 (v0.0.0 리뷰드려요. review라는 키워드를 검색하시면 찾을 수 있으실거에요.)
 
   // review
   // 콘솔로그는 디버깅용으로만 사용해주세요. 개발할 때 사용하시고 삭제해주세요.
